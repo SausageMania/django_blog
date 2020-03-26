a4ffd2e5a75ef809b288ab78ed785045f09b42d7
SausageMania <joshua5451@naver.com>
2020-03-26 14:55:22 +0900

    no message

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.BancarList.as_view(), name='bancar_list'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Bancar
+
+class BancarList(generic.ListView):
+    model = Bancar
+    template_name = 'blog/index.html'
 
 # Create your views here.
